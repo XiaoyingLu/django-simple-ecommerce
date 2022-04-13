ab7ec75ed2f914895d32309158657c89ccd39c84
Amber Lu <amber.lu@ubisoft.com>
2022-04-13 13:00:16 -0300

    fixed paths in settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -121,10 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = [BASE_DIR / "static_root"]
-MEDIA_URL = '/media'
-MEDIA_ROOT = [BASE_DIR / "media"]
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'static_root'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
